c31b81eba6c7e85d62cec417597568183664b80b
eyobeds <sebrexam2009@gmail.com>
2024-08-16 21:31:02 +0300

    new file added

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,6 +6,9 @@ from .views import index, register
 from . import views
 
 urlspattern=[
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
     path('register/', views.register, name='register'),  # views.register for user registration
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # Custom LoginView
     path('logout/', LogoutView.as_view(template_name='relationship_app/logged_out.html'), name='logout'),
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,30 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
 from .models import Library,Book
 from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseForbidden
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
 
 # Create your views here.
 def register(request):
