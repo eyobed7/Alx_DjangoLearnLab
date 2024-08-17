86d72f3a5378fcfb8da5a46d0b0979dbe7e517f6
eyobeds <sebrexam2009@gmail.com>
2024-08-17 11:59:20 +0300

    new file added

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,9 +1,9 @@
 # users/urls.py
 from django.urls import path
-from .views import admin_view, librarian_view, member_view
+from .views import Admin_view, librarian_view, member_view
 
 urlpatterns = [
-    path('admin/', admin_view, name='admin_view'),
+    path('admin/', Admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
 ]
