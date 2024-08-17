226a8180775f599c94a00268622e9c8fbd7666dc
eyobeds <sebrexam2009@gmail.com>
2024-08-17 11:28:10 +0300

    new file added

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,15 +3,12 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
 
-def is_admin(user):
+def is_Admin(user):
     return user.userprofile.role == 'Admin'
 
-@user_passes_test(is_admin)
-def admin_view(request):
+@user_passes_test(is_Admin)
+def Admin_view(request):
     return render(request, 'admin_view.html')
-
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
 
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
