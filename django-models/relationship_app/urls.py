02d0bfad95e872b2676df9b8380a39d8a6d3f382
eyobeds <sebrexam2009@gmail.com>
2024-08-18 18:42:28 +0300

    new change

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
-    path('book/add/', add_book, name='add_book'),
-    path('book/edit/<int:pk>/', edit_book, name='edit_book'),
-    path('book/delete/<int:pk>/', delete_book, name='delete_book'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
 ]
 
