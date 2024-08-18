deee9897a1faeb28e3e607da90e05001189b1bab
eyobeds <sebrexam2009@gmail.com>
2024-08-18 17:14:21 +0300

    new change has been made

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,29 +1,24 @@
-# users/views.py
-from django.contrib.auth.decorators import user_passes_test
+# relationship_app/views.py
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 
-
-def is_Admin(user):
+def is_admin(user):
     return user.userprofile.role == 'Admin'
-
-@user_passes_test(is_Admin)
-def Admin_view(request):
-    return render(request, 'admin_view.html')
 
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
 
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'librarian_view.html')
-
 def is_member(user):
     return user.userprofile.role == 'Member'
 
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
