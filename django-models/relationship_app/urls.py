cf625d1396b3cb27bac94cd62e9d3b4d1211d018
eyobeds <sebrexam2009@gmail.com>
2024-08-21 12:38:05 +0300

    new chage

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,10 +6,10 @@ from .views import add_book, edit_book, delete_book, list_books, LibraryDetailVi
 
 urlpatterns = [
     # Book-related URLs
-    path('books/', list_books, name='book_list'),  # List all books
-    path('books/add/', add_book, name='add_book'),  # Add a new book
-    path('books/edit/<int:pk>/', edit_book, name='edit_book'),  # Edit an existing book
-    path('books/delete/<int:pk>/', delete_book, name='delete_book'),  # Delete a book
+    
+    path('add_book/', add_book, name='add_book'),  # Add a new book
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),  # Edit an existing book
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),  # Delete a book
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
