c46f0d7b053ec903b4d5d8dc755c3474662f063f
eyobeds <sebrexam2009@gmail.com>
2024-08-21 11:07:29 +0300

    new change

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from .models import Library,Book
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render("relationship_app/admin_view.html")
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render("relationship_app/librarian_view.html")
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render("relationship_app/member_view.html")
 
 
 def list_books(request):
