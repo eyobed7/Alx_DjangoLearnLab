5054016ee7a165c3392b68219739e9fb06538c1e
eyobeds <sebrexam2009@gmail.com>
2024-08-22 16:50:26 +0300

    new change

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import FileExtensionValidator
+from typing import Any
+from django.db import models
+from django.contrib.auth.models import AbstractUser,UserManager,PermissionsMixin
+from django.utils import timezone
+from django.core.validators import FileExtensionValidator
 
+class CustomUserManger(UserManager):
+    def _create_user(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError("please insert your email")
+        email=self.normalize_email(email)
+        user=self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_user(self,email=None , password=None , **extra_fields) :
+        extra_fields.setdefault("is_staff",False)
+        extra_fields.setdefault("is_superuser",False)
+        return self._create_user(email,password,**extra_fields)
+    def create_superuser(self,email=None , password=None , **extra_fields) :
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+        return self._create_user(email,password,**extra_fields)
 
 class CustomUser(AbstractUser):
     date_of_birth=models.DateField(null=True, blank=True) 
