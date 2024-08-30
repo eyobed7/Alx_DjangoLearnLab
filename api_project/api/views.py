d830b101253f2f1a600e11803c2f549ec6399c7a
eyobeds <sebrexam2009@gmail.com>
2024-08-30 22:09:57 +0300

    new file added

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
-class BookViewSet(viewsets.ViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer  # Corrected here
