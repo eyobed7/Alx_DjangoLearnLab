f207f1aecd422ad3559a961133fd551c4703a436
eyobeds <sebrexam2009@gmail.com>
2024-09-05 20:38:21 +0300

    change made on the directory name

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetail.as_view(), name='book-detail'),
     path('books/create/', BookCreate.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdate.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDelete.as_view(), name='book-delete'),
+    path('books/update/<int:pk>/', BookUpdate.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDelete.as_view(), name='book-delete'),
     path("api/books", BookListCreateAPIView.as_view(), name="book_list_create"),
     path("api/Authors", AuthorListCreateAPIView.as_view(), name="author_list_create"),
 ]
