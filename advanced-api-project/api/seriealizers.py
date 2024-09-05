2315912982f41351bc5bbda19ed7722550ca0f14
eyobeds <sebrexam2009@gmail.com>
2024-09-05 09:26:31 +0300

    change made on the directory name

--- advanced-api-project/api/seriealizers.py
+++ advanced-api-project/api/seriealizers.py
@@ -14,7 +14,7 @@ class BookSerializer(serializers.ModelSerializer):
     
 
 class AuthorSerializer(serializers.ModelSerializer):
-    authors=BookSerializer(many=True,read_only=True)
+    authors=BookSerializer(many=True, read_only=True)
     class Meta:
         model=Author
         fields=['name','authors']
