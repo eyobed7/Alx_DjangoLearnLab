a639c1acf41fb23f96842113faa19180a5e96fae
eyobeds <sebrexam2009@gmail.com>
2024-09-18 16:28:40 +0300

    new change has

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 from .serializers import UserRegistrationSerializer, LoginSerializer
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
@@ -36,13 +36,13 @@ class LoginView(ObtainAuthToken):
 class ProfileView(generics.RetrieveUpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserRegistrationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         return self.request.user
     
 class FollowUserView(generics.GenericAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = CustomUser.objects.all()
    
     
@@ -59,7 +59,7 @@ class FollowUserView(generics.GenericAPIView):
         return Response({'detail': 'User followed successfully.'}, status=status.HTTP_200_OK)
 
 class UnfollowUserView(generics.GenericAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = CustomUser.objects.all()
     
     
